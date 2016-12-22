8429644a15d0d0eb4a3a48e1bd4bf682a13dc7d2
LeoDaFinchy <leodafinchy@gmail.com>
2016-12-22 20:34:52 +0000

    Created 0001

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,36 @@
 "use strict";
 
+require("babel-polyfill");
+
 import '../stylesheets/index.sass';
 import './utils/colour_console';
 import _ from 'lodash';
+
+import App from './App'
+import Room from './Room'
 
 function ready(){
   try{
     window.onload = main;
     console.log("Window found, deferring execution");
   } catch ( e ) {
-    console.log("No window, executing immediately");
-    main();
+    console.log("No window, cannot execute");
+    // main();
   }
 }
 
 ready();
 
 function main(){
-  console.log("MAIN")
+  const room1 = new Room('blue')
+  const room2 = new Room('red')
+  const room3 = new Room('green')
+  Room.connectRooms(room1, room2)
+  Room.connectRooms(room1, room3)
+
+  const app = new App({
+    currentRoom: room1
+  })
+
+  app.cycle()
 }
