0f6915ea4a7f058a990b212e94984253c78d5a13
plentific-adam <leodafinchy@gmail.com>
2017-02-16 11:39:10 +0000

    Added more attributes, weighted chance, moved rendering responsibility

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ import '../stylesheets/index.sass'
 import './utils/colour_console'
 
 import App from './App'
-import Room from './Room'
+import Room, {randomWeighted} from './Room'
 
 function ready () {
   try {
@@ -14,7 +14,27 @@ function ready () {
     console.log('Window found, deferring execution')
   } catch (e) {
     console.log('No window, cannot execute')
-    // main();
+    const tester = randomWeighted([
+      {value: 0, weight: 7},
+      {value: 1, weight: 2},
+      {value: 2, weight: 1},
+    ])
+
+    const counter = () => {
+      const counts = {}
+      return val => {
+        counts[val] = counts[val] ? counts[val] + 1 : 1
+        return counts
+      }
+    }
+
+    for (let y = 0; y < 20; y += 1) {
+      const count = counter()
+      for (let x = 0; x < 10000; x += 1) {
+        count(tester())
+      }
+      console.log(count(tester()))
+    }
   }
 }
 
